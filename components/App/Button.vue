19d8a673b1f1cf80fd4af45aedc98731a7280900
PikaDude <mail@pikadude.me>
2023-12-01 02:29:48 +1100

    add disabled mode to buttons (#11)
    it was almost used but ended up being scrapped
    but since i already made it i might as well publish it

--- components/App/Button.vue
+++ components/App/Button.vue
@@ -1,21 +1,28 @@
 <template>
-    <button class="flex items-center justify-center gap-2 px-4 py-2 font-bold text-white transition-colors rounded" :class="classes">
+    <button :disabled="disabled" class="flex items-center justify-center gap-2 text-white transition-colors rounded" :class="classes">
         <Icon v-if="icon" :name="icon" size="24" />
         <span v-if="content">{{ content }}</span>
     </button>
 </template>
 
 <script lang="ts">
-import { oneOf, string } from 'vue-types';
+import { bool, oneOf, string } from 'vue-types';
 
 export default defineComponent({
     props: {
         content: string(),
         icon: string(),
+        disabled: bool(),
         color: oneOf(['black', 'green', 'red', 'discord', 'github', 'twitter'] as const).def('green'),
+        size: oneOf(['sm', 'md'] as const).def('md'),
     },
     computed: {
         classes() {
+            return [this.sizeClasses, this.colorClasses];
+        },
+        colorClasses() {
+            if (this.disabled) return tw('bg-gray-500 cursor-not-allowed');
+
             switch (this.color) {
                 case 'black':
                     return tw('bg-black hover:bg-gray-700');
@@ -30,6 +37,15 @@ export default defineComponent({
                 case 'green':
                 default:
                     return tw('bg-green-500 hover:bg-green-600');
+            }
+        },
+        sizeClasses() {
+            switch (this.size) {
+                case 'sm':
+                    return tw('px-2 py-1 font-semibold');
+                case 'md':
+                default:
+                    return tw('px-4 py-2 font-bold');
             }
         },
     },
